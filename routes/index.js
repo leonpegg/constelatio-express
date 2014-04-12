2cba38cda1eb7fe0aab357d6395c9666a6b75102
leonpegg <leon.pegg@gmail.com>
2014-04-12 13:09:24 +0100

    navbar

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) {
     
-  res.render('homepage', {});
+  res.render('homepage', {currentUser: ''});
 });
 
 module.exports = router;
